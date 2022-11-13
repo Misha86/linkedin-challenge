c57f13f0acc7a0b00f5d670e208808b021ee4f7e
Mykhailo Polishchuk <mpolishchuk86@gmail.com>
2022-11-13 21:39:34 +0200

    Added get_puzzle_image method

--- registration_linkedin.py
+++ registration_linkedin.py
@@ -1,26 +1,18 @@
+import time
+from io import BytesIO
+
 from PIL import Image
 from selenium import webdriver
 import warnings
 import random
 import string
 import csv
-import time
 
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-
-# proxy_ip_port = "134.122.58.174:80"
-# proxy_ip_port = "117.251.103.186:8080"
-# proxy_ip_port = "157.100.12.138:999"
-# proxy_ip_port = "31.129.163.70:78"
-# proxy_ip_port = "94.45.137.34:8080"
-# proxy_ip_port = "195.138.90.226:3128"
-# proxy_ip_port = "94.45.137.34:8080"
-# proxy_ip_port = "14.140.131.82:3128"
-proxy_ip_port = ""
 
 
 class RegistrationLinkedIn:
@@ -76,37 +68,77 @@ class RegistrationLinkedIn:
         self._wait.until(EC.presence_of_element_located((By.ID, "last-name"))).send_keys(last_name)
         self._wait.until(EC.element_to_be_clickable((By.ID, "join-form-submit"))).click()
 
-    def get_capture(self):
+    def set_capture_iframe(self, cap_n=""):
         self._wait.until(EC.frame_to_be_available_and_switch_to_it((By.CLASS_NAME, "challenge-dialog__iframe")))
         self._wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, "captcha-internal")))
         self._wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, "arkoseframe")))
         self._wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, "fc-iframe-wrap")))
-        self._wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, "CaptchaFrame")))
-        self._wait.until(EC.element_to_be_clickable((By.ID, "home_children_button"))).click()
-        self._wait.until(EC.visibility_of_element_located((By.ID, "game")))
-        self._wait.until(lambda d: d.save_screenshot("./static/puzzle.png"))
-        screenshot = Image.open("./static/puzzle.png")
-        screenshot.show()
-        print(
-            """Input the image number‚ which is correctly positioned using schema!
-               _______________________
-              |       |       |       |
-              |   1   |   2   |   3   |
-              |_______|_______|_______|
-              |       |       |       |
-              |   4   |   5   |   6   |
-              |_______|_______|_______|
-            """
-        )
-        image_number = input("Input the image number: ")
+        self._wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, f"CaptchaFrame{cap_n}")))
+
+    def get_capture(self):
+        capture_num = ""
+        self.set_capture_iframe()
+        while True:
+            try:
+                self._wait.until(EC.element_to_be_clickable((By.ID, "home_children_button"))).click()
+                self._wait.until(EC.visibility_of_element_located((By.ID, "game")))
+            except TimeoutException:
+                print("Inputted wrong capture images!")
+                self._wait.until(EC.element_to_be_clickable((By.ID, "wrong_children_button"))).click()
+                capture_num = "" if capture_num else "2"
+
+            while True:
+                self._driver.switch_to.default_content()
+
+                puzzle_tag = self._wait.until(
+                    EC.visibility_of_element_located((By.XPATH, "/html/body/div[1]/main/section")))
+                puzzle_image = self.get_puzzle_image(puzzle_tag)
+
+                self.set_capture_iframe(capture_num)
+
+                try:
+                    self._wait.until(
+                        EC.visibility_of_element_located((By.XPATH, "//*[@id='game_children_challenge']/div/ul")))
+                except TimeoutException:
+                    break
+
+                puzzle_image.show()
+                print(dir(puzzle_image))
+                print(
+                    """Input the image number‚ which is correctly positioned using schema!
+                       _______________________
+                      |       |       |       |
+                      |   1   |   2   |   3   |
+                      |_______|_______|_______|
+                      |       |       |       |
+                      |   4   |   5   |   6   |
+                      |_______|_______|_______|
+                    """
+                )
+                image_number = input("Input the image number: ")
+                puzzle_image.close()
+                puzzle_image.close()
+                self._wait.until(
+                    EC.visibility_of_element_located((By.XPATH, f"//*[@id='image{image_number}']/a"))).click()
+
+    def get_puzzle_image(self, element):
+        png = self._wait.until(lambda d: d.get_screenshot_as_png())
+        screenshot = Image.open(BytesIO(png))
+        window_size = self._driver.get_window_size().values()
+        image = screenshot.resize(window_size, Image.ANTIALIAS)
+        height, width, left_pos, top_pos = element.rect.values()
+        right_pos = left_pos + width
+        bottom_pos = top_pos + height
+        puzzle = image.crop((left_pos, top_pos, right_pos, bottom_pos))  # defines crop points
+        return puzzle
 
     def create(self):
         data = self.get_username_password()
         if data:
             first_name = self.random_string()
             last_name = self.random_string()
-            self.set_driver(proxy_ip_port)
-            self.set_wait_time(20)
+            self.set_driver()
+            self.set_wait_time(5)
             self.get_method_from_url()
             self.fill_main_form(*data)
             self.fill_first_fast_names(first_name, last_name)
